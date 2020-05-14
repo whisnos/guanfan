2d3b6d658581b4caa61520715933b4553484d35a
huang <kaixin654321@gmail.com>
2020-05-14 17:43:15 +0800

    九、热搜关键词 1) 添加热搜词，根据搜索次数动态更新 2) 后台内容管理页中，添加热搜管理栏目，可手动更改​​

---
+++ cmshtml/src/controller/content/trending/list.js
@@ -0,0 +1,165 @@
+layui.define(['table', 'form', 'layer'], function (exports) {
+    var $ = layui.$
+      , admin = layui.admin
+      , view = layui.view
+      , table = layui.table
+      , layer = layui.layer
+      , form = layui.form;
+  
+
+    //动态列表
+    table.render({
+      elem: '#LAY-cms-content-trending-list'
+      , url: '/api/trending/list' //获取数据接口
+      , cols: [[
+        // {type: 'checkbox', fixed: 'left'}
+       { field: 'id', title: 'ID', minWidth: 80 }
+      , { field: 'keyword', title: '热搜词', minWidth: 80, align: 'center' }
+      , { field: 'visitCount', title: '浏览量', minWidth: 80, align: 'center' }
+        // , { field: 'trendingId', title: '主题ID', minWidth: 80 ,templet: '#content_trending_id_Tpl', align: 'center' }
+        // , { field: 'title', title: '主题标题', minWidth: 200 }
+        // , { field: 'recipeNum', title: '食谱数量', minWidth: 100, sort: true }
+        // , { field: 'reason', title: '理由', minWidth: 200,width:300, sort: true }
+        , { field: 'sort', title: '排序', minWidth: 100, sort: true }
+        , { field: 'status', title: '状态', templet: '#trending_status_Tpl', minWidth: 100, align: 'center' }
+        , { field: 'updateTime', title: '更新时间', sort: true, minWidth: 180 }
+        , { field: 'createTime', title: '上传时间', sort: true, minWidth: 180 }
+        , { title: '操作', minWidth: 200, align: 'center', fixed: 'right', toolbar: '#toolbar-trending-type-content-list' }
+      ]]
+      , page: true
+      , limit: 10
+      , limits: [10, 15, 20, 25, 30]
+      , text: '对不起，加载出现异常！'
+    });
+
+    form.render(null, 'cms-content-trending-search');
+    //监听搜索
+    form.on('submit(LAY-cms-content-trending-search)', function (data) {
+      var field = data.field;
+      table.reload('LAY-cms-content-trending-table-list', {
+        where: field,
+        page: {
+          curr: 1 //重新从第 1 页开始
+        }
+      });
+    });
+
+    //监听工具条
+    table.on('tool(LAY-cms-content-trending-list)', function (obj) {
+        var data = obj.data;
+        if  (obj.event === 'trending_type_edit'){
+            admin.popup({
+                title: '编辑热搜主题'
+                , area: ['450px', '450px']
+                , id: 'LAY-popup-content-trending-edit-form'
+                , success: function (layero, index) {
+                    view(this.id).render('app/content/trending/edit', data).done(function(){
+                    submit_trending_edit_form(index);
+                });
+                }
+            });
+        } else if(obj.event === 'trending_type_del'){
+            // 删除
+            layer.confirm('确定删除吗？', function (index) {
+                trending_del({'id':data.id});
+            });
+        }
+    });
+
+    var active = {
+        trending_type_add:function (othis) {
+            // 添加分类
+            admin.popup({
+                title: '添加热搜主题'
+                , area: ['450px', '450px']
+                , id: 'LAY-popup-content-trending-edit-form-add'
+                , success: function (layero, index) {
+                    view(this.id).render('app/content/trending/edit').done(function(){
+                    submit_trending_edit_form(index);
+                });
+                }
+            });
+        }
+    };
+
+    $('.layui-btn.layuiadmin-btn-list').on('click', function () {
+    var type = $(this).data('type');
+    active[type] ? active[type].call(this) : '';
+    });
+
+    function submit_trending_edit_form(realindex){
+        // 编辑和添加监听
+        form.on('submit(cms-content-trending-edit-form-submit)', function (data) {
+            var field = data.field;
+            console.log(field);
+            if(field.id==''){
+                //就是添加
+                trending_add(field);
+            }else{
+                trending_edit(field);
+            }
+            layer.close(realindex); //执行关闭
+        });
+    }
+
+    function trending_add(data){
+        // 添加
+        admin.req({
+            type: 'post',
+            url: '/api/trending/add', //添加
+            data: data,
+            success: function (result) {
+                if (result.success) {
+                } else {
+                    layer.msg(result.msg)
+                }
+            },
+            complete:function (result) {
+                table.reload('LAY-cms-content-trending-list');
+            },
+            error: function (error) {
+            }
+        });
+    }
+
+    function trending_edit(data){
+        // 设置设置状态
+        admin.req({
+            type: 'post',
+            url: '/api/trending/edit',
+            data: data,
+            success: function (result) {
+                if (result.success) {
+                } else {
+                    layer.msg(result.msg)
+                }
+            },
+            complete:function (result) {
+                table.reload('LAY-cms-content-trending-list');
+            },
+            error: function (error) {
+            }
+        });
+    }
+    function trending_del(data){
+        // 设置
+        admin.req({
+            type: 'post',
+            url: '/api/trending/del', //添加
+            data: data,
+            success: function (result) {
+                if (result.success) {
+                } else {
+                    layer.msg(result.msg)
+                }
+            },
+            complete:function (result) {
+                table.reload('LAY-cms-content-trending-list');
+            },
+            error: function (error) {
+            }
+        });
+    }
+
+    exports('content/trending/list', {})
+});
